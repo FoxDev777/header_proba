ae264bdf12a3f39a4e6f18695c6e727e79c3c57d
Wolodymyr Magera <foxwest@ukr.net>
2023-09-29 20:39:07 +0200

    headerMobile&&slider

--- src/components/SimpleSlider/SimpleSlider.jsx
+++ src/components/SimpleSlider/SimpleSlider.jsx
@@ -7,7 +7,6 @@ import "slick-carousel/slick/slick-theme.css";
 import "./simpleSlider.css";
 
 export default function SimpleSlider() {
-
   const settings = {
     dots: true,
     infinite: true,
@@ -17,32 +16,57 @@ export default function SimpleSlider() {
     speed: 500,
     autoplaySpeed: 3000,
     pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          centerMode: true,
+          centerPadding: "10px",
+          mobileFirst: true,
+          arrows: false,
+        },
+      },
+    ],
   };
 
   return (
     <>
-    <Slider {...settings}>
-      {dataSlider.map((elem) => {
-        return (
-          <div key={elem.id}>
-            <div className="sliderWrapper">
-              <h3>{elem.text_1}</h3>
-              <img
-                onClick={() => window.open(`${elem.url}`)}
-                width={17}
-                // height={10}
-                className="imageSlider"
-                src={elem.img}
-                alt="imageSlider"
-              />
-              <p className="sliderTextAbout">
-                <span>{elem.text_2title}</span> : {elem.text_2}
-              </p>
+      <Slider {...settings}>
+        {dataSlider.map((elem) => {
+          return (
+            <div key={elem.id}>
+              <div className="sliderWrapper">
+                <h3>{elem.text_1}</h3>
+                <img
+                  onClick={() => window.open(`${elem.url}`)}
+                  width={17}
+                  // height={10}
+                  className="imageSlider"
+                  src={elem.img}
+                  alt="imageSlider"
+                />
+                <p className="sliderTextAbout">
+                  <span>{elem.text_2title}</span> : {elem.text_2}
+                </p>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </Slider>
+          );
+        })}
+      </Slider>
     </>
   );
 }
